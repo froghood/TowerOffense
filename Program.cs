8d51e4a6bf9fc9adacd2bec38173577f56f5d1a2
Alex Mike Mark <alex.mike.mark@gmail.com>
2022-08-22 13:49:40 -0500

    Make the program old again.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace TowerOffense {
+    class Program {
+        static void Main(string[] args) {
+            System.Console.WriteLine("WOW");
+        }
+    }
+}
