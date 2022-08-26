397af0f86e7d63a6a85a5f111cef89431e6b5601
frog <frog@gmail.com>
2022-08-26 17:59:00 -0400

    stubbies

---
+++ TowerOffense.Scenes/Gameplay/Objects/Enemy.cs
@@ -0,0 +1,10 @@
+using Microsoft.Xna.Framework;
+using TowerOffense.Objects.Common;
+
+namespace TowerOffense.Scenes.Gameplay.Objects {
+    public class Enemy : Entity {
+        public Enemy(Scene scene, Point position, Point size, int titleBarHeight = 24, int borderThickness = 1) : base(scene, position, size, titleBarHeight, borderThickness) {
+            // STUB
+        }
+    }
+}
