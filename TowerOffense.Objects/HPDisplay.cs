3d8ba2fe23ead065b70bedff75e31524efc3f340
Alex Mike Mark <alex.mike.mark.again@gmail.com>
2022-08-26 10:33:33 -0500

    Renders the text less stupidly

--- TowerOffense.Objects/HPDisplay.cs
+++ TowerOffense.Objects/HPDisplay.cs
@@ -1,15 +1,27 @@
 using Microsoft.Xna.Framework;
 using TowerOffense.Objects.Base;
 using TowerOffense.Scenes;
+using Microsoft.Xna.Framework.Graphics;
+
 namespace TowerOffense.Objects {
     public class HPDisplay : SceneWindow {
-        private PlayerManager PlayerManager = PlayerManager.Instance;
-        public HPDisplay(Scene scene, Point position, Point size) : base(scene, position, size) { }
+        private PlayerManager _playerManager = PlayerManager.Instance;
+        private SpriteFont _font;
+        private string _text;
+
+        public HPDisplay(Scene scene, Point position, Point size) : base(scene, position, size) {
+            _font = TOGame.Instance.Content.Load<SpriteFont>("Fonts/HpDisplay");
+        }
 
         public override void Update(GameTime gameTime){
-            this.Text = PlayerManager.HP.ToString();
+            _text = _playerManager.HP.ToString();
         }
         public override void Render(GameTime gameTime){
+            Vector2 textMiddlePoint = _font.MeasureString(_text) / 2;
+            Vector2 position = new Vector2(16,16); // some work to center this would be cool
+            TOGame.SpriteBatch.DrawString(_font, _text, position, Color.Red);
         }
     }
 }
+
+// other font drawing info https://docs.monogame.net/articles/content/adding_ttf_fonts.html
--- TowerOffense.Scenes/HPTestScene/Objects/HPKiller.cs
+++ TowerOffense.Scenes/HPTestScene/Objects/HPKiller.cs
@@ -5,17 +5,17 @@ using TowerOffense.Objects.Base;
 namespace TowerOffense.Scenes.HPTest.Objects {
     public class HPKiller : SceneObject {
 
-        private PlayerManager _pm = PlayerManager.Instance; // christ rename
+        private PlayerManager _playerManager = PlayerManager.Instance;
         private SceneWindow _window;
         private double timeCount = 0;
-        private double timeLimit = 2;
+        private double timeLimit = 1;
 
         public HPKiller(Scene scene) : base(scene) { }
 
         public override void Update(GameTime gameTime) {
             timeCount += gameTime.ElapsedGameTime.TotalSeconds;
             if ( timeCount > timeLimit ){
-                _pm.SubtractHP(5);
+                _playerManager.SubtractHP(5);
                 timeCount = 0;
             }
         }
